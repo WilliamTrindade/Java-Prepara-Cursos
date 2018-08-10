77f2183ee701f045f3856f9ae1ac18766322239e
William Trindade <williamtrindade777@gmail.com>
2018-08-10 14:45:10 -0300

    Add files via upload

---
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,14 @@
+package olamundo;
+public class OlaMundo {
+//automaticamente apps começam executando em main, 
+//apos temo os método
+//void indica que ele não retornara nada apos realizar a tarefa
+//static define que todos objetos da classe compartilhem a mesma parte de dados
+	public static void main (String args[])
+	{
+		System.out.println("Olá, Mundo!");
+//classe system do pacote java.lan executa o método exit com arg 0 . 		
+		System.exit(0);
+	}
+}
+
